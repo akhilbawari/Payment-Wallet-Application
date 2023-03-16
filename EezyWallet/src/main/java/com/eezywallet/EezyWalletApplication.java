b8a9994e57e56d5611e6dbb6aa526e51eecafd32
akhilbawari <akhilbawari0804@gmail.com>
2023-03-16 16:04:02 +0530

    added application folder

---
+++ EezyWallet/src/main/java/com/eezywallet/EezyWalletApplication.java
@@ -0,0 +1,13 @@
+package com.eezywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EezyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EezyWalletApplication.class, args);
+	}
+
+}
---
+++ EezyWallet/src/test/java/com/eezywallet/EezyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eezywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EezyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
